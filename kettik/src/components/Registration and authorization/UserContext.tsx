94dae6673eb8e37d21c5a9a3f9bd3d6e21f217ce
Assol Rakhimova <assoliarakhimova@gmail.com>
2023-03-09 14:59:45 +0000

    Upload New File

---
+++ kettik/src/components/Registration and authorization/UserContext.tsx
@@ -0,0 +1,56 @@
+import {
+    createContext,
+    PropsWithChildren,
+    useContext,
+    useEffect,
+    useState,
+  } from 'react';
+  import { authService } from './index';
+  
+  export interface UserContexState {
+    username: string;
+    changeUsername: (v: string) => void;
+  }
+  
+  export const UserContext = createContext<UserContexState | undefined>(
+    undefined
+  );
+  
+  export const UserContextProvider: React.FC<PropsWithChildren> = ({
+    children,
+  }) => {
+    const [username, setUsername] = useState('');
+  
+    useEffect(() => {
+      if (!authService.getIdToken()) {
+        return;
+      }
+      authService.getProfile().then((r) => {
+        setUsername(r.email);
+      });
+    }, []);
+  
+    const handleUsernameChange = (v: string) => {
+      setUsername(v);
+    };
+  
+    return (
+      <UserContext.Provider
+        value={{
+          username,
+          changeUsername: handleUsernameChange,
+        }}
+      >
+        {children}
+      </UserContext.Provider>
+    );
+  };
+  
+  export const useUserContext = () => {
+    const context = useContext(UserContext);
+    if (typeof context === 'undefined') {
+      throw Error('User context is not defined');
+    }
+    return context;
+  };
+  
