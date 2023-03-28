dcba26251fe74667f4aa6d6f1a4aa7697dbf5a94
Miras Magzom <mmagzom@dar.kz>
2023-03-28 16:36:51 +0600

    fix(): event create request format

--- src/pages/CreateEventPage/CreateEventPage.tsx
+++ src/pages/CreateEventPage/CreateEventPage.tsx
@@ -1,33 +1,37 @@
-import { NavLink } from "react-router-dom";
-import { CreateEventForm } from "../../components/Forms/CreateEventForm/CreateEventForm"
-import { createEvent } from "../../services/events";
-import { PostFormValues } from "../../types/event"
+import { NavLink } from 'react-router-dom';
+import { CreateEventForm } from '../../components/Forms/CreateEventForm/CreateEventForm';
+import { createEvent } from '../../services/events';
+import { PostFormValues } from '../../types/event';
 import styles from './CreateEventPage.module.css';
 
-
-
-export const CreateEventPage = () =>{
-
-    const handleSubmit = (data:PostFormValues,photo:File) =>{
+export const CreateEventPage = () => {
+    const handleSubmit = (data: PostFormValues, photo: File) => {
         let formData = new FormData();
-        formData.append('event', JSON.stringify(data))
-        formData.append('photo',photo)
+        const json = JSON.stringify({
+            name: 'event_1',
+            description: 'description_1',
+            category: 'CULTURAL',
+            location: 'location_1',
+            dateTime: '2023-04-05T14:30',
+        });
+        const blob = new Blob([json], { type: 'application/json' });
+        formData.append('event', blob, '');
+        formData.append('photos', photo, photo.name);
         createEvent(formData);
-        console.log(data,photo);
-        
-    }
+        console.log(data, photo);
+    };
     return (
         <>
-        <div className={styles.EventFormContainer}>
-            <div className={styles.headerContainer}>
-            <NavLink to={'/'} className={styles.navLinkContainer}>
-            <img src="/assets/header/arrowLeft.svg" alt="arrow" />
-            <p className={styles.title}>  Вернуться на главную</p></NavLink>
-            </div>
+            <div className={styles.EventFormContainer}>
+                <div className={styles.headerContainer}>
+                    <NavLink to={'/'} className={styles.navLinkContainer}>
+                        <img src='/assets/header/arrowLeft.svg' alt='arrow' />
+                        <p className={styles.title}> Вернуться на главную</p>
+                    </NavLink>
+                </div>
 
-            <CreateEventForm onSubmit={handleSubmit}/>
-        </div>  
+                <CreateEventForm onSubmit={handleSubmit} />
+            </div>
         </>
-    
-    )
-}
+    );
+};
--- src/services/events.ts
+++ src/services/events.ts
@@ -1,123 +1,99 @@
 import axios from 'axios';
 import { commets, Events } from '../types/event';
 
-const eventApi = process.env.REACT_APP_API_ROOT_EVENT as string
+const eventApi = process.env.REACT_APP_API_ROOT_EVENT as string;
 
-const commentApi = process.env.REACT_APP_API_ROOT_COMMENT as string
+const commentApi = process.env.REACT_APP_API_ROOT_COMMENT as string;
 
-
-  export const getEvents = (filter:string) => {
+export const getEvents = (filter: string) => {
     return axios
-      .get(`${eventApi}/event/${filter}`, {})
-      .then((res) => res.data)
-      .then<Events[]>((res)=>res.content)
+        .get(`${eventApi}/event/${filter}`, {})
+        .then((res) => res.data)
+        .then<Events[]>((res) => res.content);
+};
 
-  };
-
-
-export const getEvent = (id:string) =>{
+export const getEvent = (id: string) => {
     return axios
         .get<Events>(`${eventApi}/event/${id}`, {})
-        .then((res)=>res.data);
-}
+        .then((res) => res.data);
+};
 
-export const createEvent = (data:FormData) => {
+export const createEvent = (data: FormData) => {
+    return axios.post(`${eventApi}/event`, data).then((res) => res);
+};
 
-    return axios
-      .post(`${eventApi}/event`,{
-        data,
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      })
-      .then((res) => res);
-  };
+export const updateEvent = (id: string, data: Events) => {
+    return axios.put(`${eventApi}/event/${id}`, data).then((res) => res.data);
+};
 
-  export const updateEvent = (id: string, data:Events) => {
-    return axios
-      .put(`${eventApi}/event/${id}`, data)
-      .then((res) => res.data);
-  };
-
-  export const deleteEvent = (id:string) =>{
+export const deleteEvent = (id: string) => {
     return axios
         .delete<Events>(`${eventApi}/event/${id}`, {})
-        .then((res)=>res);
-}
+        .then((res) => res);
+};
 
-
-  export const getComments = (id:string) => {
+export const getComments = (id: string) => {
     return axios
-      .get(`${commentApi}/event/${id}/comment/all`, {})
-      .then((res) => res.data)
-      .then<commets[]>((res)=>res.content)
-  };
+        .get(`${commentApi}/event/${id}/comment/all`, {})
+        .then((res) => res.data)
+        .then<commets[]>((res) => res.content);
+};
 
-  export const createComment = (id:string,data:commets) => {
+export const createComment = (id: string, data: commets) => {
     return axios
-      .post(`${commentApi}/event/${id}/comment`, data)
-      .then((res) => res.data);
-  };
-  
+        .post(`${commentApi}/event/${id}/comment`, data)
+        .then((res) => res.data);
+};
 
-  export const temp =[
+export const temp = [
     {
-        id: "AANd2ds4YdsaBtFDbdsaKLnbdsadSSpV",
-        name: "Music Festival",
-        description: "A music festival featuring local bands",
-        category: "EDUCATIONAL",
-        location: "Central Park",
+        id: 'AANd2ds4YdsaBtFDbdsaKLnbdsadSSpV',
+        name: 'Music Festival',
+        description: 'A music festival featuring local bands',
+        category: 'EDUCATIONAL',
+        location: 'Central Park',
         likes: 0,
-        dateTime: "2023-03-15T16:30",
-        userEmail: "John Doe",
-        dateOfCreation: "2023-03-13T20:26:09.310",
-        imageUrls: [
-            "/assets/KETTIK.svg"     
-           ]
+        dateTime: '2023-03-15T16:30',
+        userEmail: 'John Doe',
+        dateOfCreation: '2023-03-13T20:26:09.310',
+        imageUrls: ['/assets/KETTIK.svg'],
     },
     {
-        id: "AANd24YdsaBtFDbdsaKLnbdsadSSpV",
-        name: "Music Festival",
-        description: "A music festival featuring local bands",
-        category: "EDUCATIONAL",
-        location: "Central Park",
+        id: 'AANd24YdsaBtFDbdsaKLnbdsadSSpV',
+        name: 'Music Festival',
+        description: 'A music festival featuring local bands',
+        category: 'EDUCATIONAL',
+        location: 'Central Park',
         likes: 0,
-        dateTime: "2023-03-11T16:30",
-        userEmail: "John Doe",
-        dateOfCreation: "2023-03-13T20:26:09.310",
-        imageUrls: [
-            "/assets/KETTIK.svg"     
-           ]
-    },
-  
-    {
-        id: "AANd24YBtFDdsabdsaKLnbdsadSSpV",
-        name: "DADSDA321",
-        description: "A music festival featuring local bands",
-        category: "EDUCATIONAL",
-        location: "Central Park",
-        likes: 0,
-        dateTime: "2023-03-21T16:30",
-        userEmail: "dias@mail.ru",
-        dateOfCreation: "2023-03-13T20:26:09.310",
-        imageUrls: [
-            "/assets/KETTIK.svg"     
-           ]
+        dateTime: '2023-03-11T16:30',
+        userEmail: 'John Doe',
+        dateOfCreation: '2023-03-13T20:26:09.310',
+        imageUrls: ['/assets/KETTIK.svg'],
     },
 
     {
-        id: "AANd24YBtFDbdsdsafgaKLnbdsadSSpV",
-        name: "dasdasd",
-        description: "A music festival featuring local bands",
-        category: "EDUCATIONAL",
-        location: "Central Park",
+        id: 'AANd24YBtFDdsabdsaKLnbdsadSSpV',
+        name: 'DADSDA321',
+        description: 'A music festival featuring local bands',
+        category: 'EDUCATIONAL',
+        location: 'Central Park',
         likes: 0,
-        dateTime: "2023-03-11T16:30",
-        userEmail: "dias@mail.ru",
-        dateOfCreation: "2023-03-13T20:26:09.310",
-        imageUrls: [
-            "/assets/KETTIK.svg"     
-           ]
-    }
-    
-  ]
+        dateTime: '2023-03-21T16:30',
+        userEmail: 'dias@mail.ru',
+        dateOfCreation: '2023-03-13T20:26:09.310',
+        imageUrls: ['/assets/KETTIK.svg'],
+    },
+
+    {
+        id: 'AANd24YBtFDbdsdsafgaKLnbdsadSSpV',
+        name: 'dasdasd',
+        description: 'A music festival featuring local bands',
+        category: 'EDUCATIONAL',
+        location: 'Central Park',
+        likes: 0,
+        dateTime: '2023-03-11T16:30',
+        userEmail: 'dias@mail.ru',
+        dateOfCreation: '2023-03-13T20:26:09.310',
+        imageUrls: ['/assets/KETTIK.svg'],
+    },
+];
